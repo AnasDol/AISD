bc17c2909311f07618b764281f6c3ab52750940e
AnasDol <anastasiadol2002@gmail.com>
2022-09-22 19:23:52 +0700

    initial commit

---
+++ List.cpp
@@ -0,0 +1,566 @@
+﻿
+#include <iostream>
+// 5. Структура данных - двусвязная, на базе адресных указателей.
+
+
+template <typename T>
+
+class List {
+
+protected:
+
+    class Node {
+
+    public:
+
+        T value;
+        Node* next;
+        Node* prev;
+
+        Node(T value) {
+            this->value = value;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+
+    };
+
+    Node* head;
+    Node* tail;
+    int size;
+
+    int stepCount;
+
+public:
+
+    List(); // конструктор
+    List(const List<T>& list); // конструктор копирования
+    ~List(); // деструктор
+
+    int getSize(); // опрос размера списка
+    void clear(); // очистка списка
+    bool isEmpty(); // проверка списка на пустоту
+
+    bool isExisting(T value); // опрос наличия заданного значения
+    T read(int index); // чтение значения с заданным номером в списке
+    bool update(int index, T newval); // изменение значения с заданным номером в списке
+
+    int getIndex(T value); // получение позиции в списке для заданного значения
+
+    void add(T value); // включение нового значения, 
+    bool add(T value, int index); // включение нового значения в позицию с заданным номером
+
+    // проблема - при T = int перегрузка некорректна, костыль = ненужная переменная
+    bool remove(T value); //удаление заданного значения из списка
+    bool remove(int index, bool b); // удаление значения из позиции с заданным номером
+
+    void show(); //  оставлено только для тестирования
+    int getStepCount() { return stepCount; }
+
+
+    class Iterator
+    {
+
+    private:
+
+        Node* cur;
+
+    public:
+
+        Iterator(Node* ptr = nullptr) {
+            cur = ptr;
+        }
+        Iterator(const Iterator& iterator) {
+            cur = iterator.cur;
+        }
+
+        bool operator==(const Iterator& lhs) const
+        {
+            return (cur == lhs.cur);
+        }
+
+        bool operator!=(const Iterator& lhs) const
+        {
+            return (cur != lhs.cur);
+        }
+
+        Iterator operator++()
+        {
+            if (cur == nullptr) throw std::exception("exception");
+            cur = cur->next;
+            return *this;
+            
+        }
+
+        Iterator operator--()
+        {
+            if (cur == nullptr) throw std::exception("exception");
+            cur = cur->prev;
+            return *this;
+        }
+
+        T& operator*() const {
+            if (cur == nullptr) throw std::exception("exception");
+            return cur->value;
+        }
+
+    };
+
+
+    class ReverseIterator
+    {
+
+    private:
+
+        Node* cur;
+
+    public:
+
+        ReverseIterator(Node* ptr = nullptr) {
+            cur = ptr;
+        }
+        ReverseIterator(const ReverseIterator& iterator) {
+            cur = iterator.cur;
+        }
+
+        bool operator==(const ReverseIterator& lhs) const
+        {
+            return (cur == lhs.cur);
+        }
+
+        bool operator!=(const ReverseIterator& lhs) const
+        {
+            return (cur != lhs.cur);
+        }
+
+        ReverseIterator operator++()
+        {
+            if (cur == nullptr) throw std::exception("exception");
+            cur = cur->prev;
+            return *this;
+        }
+
+        ReverseIterator operator--()
+        {
+            if (cur == nullptr) throw std::exception("exception");
+            cur = cur->next;
+            return *this;
+        }
+
+        T& operator*() const {
+            if (cur == nullptr) throw std::exception("exception");
+            return cur->value;
+        }
+
+    };
+
+    friend class Iterator;
+    friend class ReverseIterator;
+
+    using iterator = Iterator;
+    using reverse_iterator = ReverseIterator;
+    
+    iterator begin() {
+        if (head != nullptr) return iterator(head);
+        else return iterator();
+    }
+    iterator end() {
+        if (tail != nullptr) return iterator(tail->next);
+        else return iterator();
+    }
+
+    reverse_iterator rbegin() {
+        if (tail != nullptr) return reverse_iterator(tail);
+        else return reverse_iterator();
+    }
+    reverse_iterator rend() {
+        if (head != nullptr) return reverse_iterator(head->prev);
+        else return reverse_iterator();
+    }
+
+
+};
+
+    template<typename T> List<T>::List()  // конструктор
+    {
+        head = NULL;
+        tail = NULL;
+        size = 0;
+    }
+
+    template<typename T> List<T>::List(const List<T>& list) // конструктор копирования
+    {
+        head = tail = NULL;
+        size = 0;
+        Node* p = list.head;
+        while (p != NULL) {
+            this->add(p->value);
+            p = p->next;
+        }
+    }
+
+    template<typename T> List<T>::~List() // деструктор
+    {
+        clear();
+    }
+
+    template<typename T> int List<T>::getSize() // опрос размера списка
+    {
+        return size;
+    }
+
+    template<typename T> void List<T>::clear() // очистка списка
+    {
+        
+        while (head)  //Пока по адресу на начало списка что-то есть
+        {
+            tail = head->next; //Резервная копия адреса следующего звена списка
+            delete head;  //Очистка памяти от первого звена
+            head = tail;  //Смена адреса начала на адрес следующего элемента
+        }
+        size = 0;
+
+    }
+
+    template<typename T> bool List<T>::isEmpty() // проверка на пустоту
+    {
+        return (head == NULL && tail == NULL && size == 0);
+    }
+
+    template<typename T> bool List<T>::isExisting(T value) // опрос наличия заданного значения
+    {
+        Node* p = head;
+        stepCount = 0;
+        while (p != NULL) {
+            if (p->value == value) return true;
+            p = p->next;
+            stepCount++;
+        }
+        return false;
+    }
+
+    template<typename T> T List<T>::read(int index) // чтение значения с заданным номером в списке
+    {
+        if (!(index >= 0 && index < size)) throw std::exception("exception");
+
+        /*Node* p;
+        if (size >> 1 > index) {
+            p = head;
+            for (int i = 1; i != index; i++) p = p->next;
+        }
+        else {
+            p = tail;
+            for (int i = size; i != index; i--) p = p->prev;
+        }*/
+
+        Node* p;
+        if (index <= size - 1 - index) // ближе с головы
+        {
+            p = head;
+            for (int i = 0; i < index; i++) p = p->next;
+        }
+        else // ближе с хвоста
+        {
+            p = tail;
+            for (int i = size - 1; i > index; i--) p = p->prev;
+        }
+        // остановились на элементе, который должны подвинуть вправо, чтобы вставить новый на его место
+
+        return p->value;
+    }
+
+    template<typename T> bool List<T>::update(int index, T newval) // изменение значения с заданным номером в списке
+    {
+
+        if (!(index >= 0 && index < size)) return false;
+
+        /*Node* p;
+        if (size >> 1 > index) {
+            p = head;
+            for (int i = 1; i != index; i++) p = p->next;
+        }
+        else {
+            p = tail;
+            for (int i = size; i != index; i--) p = p->prev;
+        }*/
+
+        Node* p;
+        if (index <= size - 1 - index) // ближе с головы
+        {
+            p = head;
+            for (int i = 0; i < index; i++) p = p->next;
+        }
+        else // ближе с хвоста
+        {
+            p = tail;
+            for (int i = size - 1; i > index; i--) p = p->prev;
+        }
+        // остановились на элементе, который должны подвинуть вправо, чтобы вставить новый на его место
+
+        p->value = newval;
+        return true;
+    }
+
+    template<typename T> int List<T>::getIndex(T value) // получение позиции в списке для заданного значения
+    {
+        Node* p = head;
+        int index = 0;
+        while (p != NULL) {
+            if (p->value == value) return index;
+            p = p->next;
+            index++;
+        }
+        return -1;
+    }
+
+    template<typename T> void List<T>::add(T value) // включение нового значения
+    {
+        Node* p = new Node(value);
+
+        //step = 0;
+        if (head != NULL) {
+            //step++;
+            p->prev = tail;
+            tail->next = p;
+        }
+        else { 
+            p->prev = NULL;  
+            head = p;
+        }
+
+        tail = p;
+        size++;
+    }
+
+    template<typename T> bool List<T>::add(T value, int index) // включение нового значения в позицию с заданным номером 
+    {
+
+        if (!(index >= 0 && index <= size)) return false; // индекс некорректен
+        else if (index == size) {
+            add(value);
+            return true;
+        }
+
+        Node* newNode = new Node(value);
+        
+        /*Node* p;
+        if (size >> 1 > index) {
+            p = head;
+            for (int i = 1; i != index; i++) p = p->next;
+        }
+        else {
+            p = tail;
+            for (int i = size; i != index; i--) p = p->prev;
+        } // остановились на элементе, который должны подвинуть вправо, чтобы вставить новый на его место*/
+
+        Node* p;
+        stepCount = 0;
+        if (index <= size - 1 - index) // ближе с головы
+        {
+            p = head;
+            for (int i = 0; i < index; i++) {
+                p = p->next;
+                stepCount++;
+            }
+        }
+        else // ближе с хвоста
+        {
+            p = tail;
+            for (int i = size - 1; i > index; i--) {
+                p = p->prev;
+                stepCount++;
+            }
+        }
+        // остановились на элементе, который должны подвинуть вправо, чтобы вставить новый на его место
+
+        if (p == head) {
+            newNode->next = p;
+            p->prev = newNode;
+            head = newNode;
+        }
+        else {
+
+            p->prev->next = newNode;
+            newNode->prev = p->prev;
+            newNode->next = p;
+            p->prev = newNode;
+        }
+
+        size++;
+        return true;
+
+        /*
+        if (!isNumCorrect(n)) {
+            if (n != size + 1) return false; //åñëè íîìåð âíå ðàçìåðíîñòè ñïèñêà è íå ðàâåí (size+1), âîçâðàùàåì false
+            else { //åñëè íîìåð = (size+1), âûçûâàåì ìåòîä äîáàâëåíèÿ çàïèñè â êîíåö ñïèñêà
+                add(obj);
+                return true;
+            }
+        }
+        if (n == 1) { //åñëè íîìåð = 1, äîáàâëÿåì ýëåìåíò â ñàìîå íà÷àëî
+            Record* nd = new Record;
+            nd->prev = NULL;
+            nd->next = first;
+            nd->value = obj;
+            first->prev = nd;
+            first = nd;
+            step = 1;
+            size++;
+            return true;
+        }
+        Record* tmp = first;
+        for (int i = 1; i < n; i++) tmp = tmp->next; // óñòàíàâëèâàåì óêàçàòåëü íà ñëåäóþùèé ýëåìåíò (äî ââåä¸ííîãî íîìåðà)
+        Record* ptmp = tmp->prev; //ýëåìåíò, èäóùèé ïåðåä tmp
+        Record* nd = new Record; //íîâûé ýëåìåíò
+        nd->value = obj;
+        if (ptmp != 0 && size != 1) ptmp->next = nd;
+        nd->next = tmp;
+        nd->prev = ptmp;
+        tmp->prev = nd;
+        step = n >> 1;
+        size++;
+        return true;*/
+    }
+
+    template<typename T> bool List<T>::remove(T value) {
+        
+        int index = getIndex(value); // находим индекс нужного элемента
+        if (index == -1) return false; // нет такого элемента
+
+        return remove(index, true);
+
+        /*Node p;
+        if (size >> 1 > index) {
+            p = head;
+            for (int i = 1; i != index; i++) p = p->next;
+        }
+        else {
+            p = tail;
+            for (int i = size; i != index; i--) p = p->prev;
+        } // остановились на элементе, который должны удалить
+
+        if (p == head) {
+            head = p->next;
+            p->next->prev = NULL;
+        }
+        else if (p == tail) {
+            tail = p->prev;
+            p->prev->next = NULL;
+        }
+        else {
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+        }
+
+        delete p;
+        size--;
+        return true;*/
+        
+       
+        /*step = 0;
+        Record* nd = first; //ïðèñâàèâàåì àäðåñ ïåðâîãî ýëåìåíòà
+        while (nd != NULL) { //ïîêà nd íå ïóñòîå
+            step++;
+            if (nd->value == obj) { //ïðîâåðÿåì çíà÷åíèå çàïèñè íà ðàâåíñòâî èñêîìîé çàïèñè
+                Record* tn = nd->next;
+                Record* tp = nd->prev;
+                if (tp == NULL) first = tn; //óäàëÿåìûé áûë ïåðâûì
+                if (tn == NULL) last = tp; //óäàëÿåìûé áûë ïîñëåäíèì
+                if (tp != NULL) tp->next = tn; //ñóùåñòâóåò ïðåäûäóùèé - óáèðàåì ñâÿçü ñ óäàëÿåìûì
+                if (tn != NULL) tn->prev = tp; //ñóùåñòâóåò ñëåäóþùèé - óáèðàåì ñâÿçü ñ óäàëÿåìûì
+                delete nd; //î÷èñòêà ïàìÿòè
+                size--; //ðàçìåðíîñòü -1
+                return true; //âîçâðàò true â ñëó÷àå óñïåõà
+            }
+            else nd = nd->next; //åñëè çíà÷åíèå íå ðàâíî èñêîìîìó, ïðîäîëæàåì ïåðåáîð
+        }
+        return false; //âîçâðàò false â ñëó÷àå íåóäà÷è îïåðàöèè óäàëåíèÿ*/
+    }
+
+    template<typename T> bool List<T>::remove(int index, bool b) {
+        
+        if (!(index >= 0 && index < size)) return false;
+        if (size == 1) {
+            clear();
+            return true;
+        }
+
+        /*Node p;
+        if (size >> 1 > index) {
+            p = head;
+            for (int i = 1; i != index; i++) p = p->next;
+        }
+        else {
+            p = tail;
+            for (int i = size; i != index; i--) p = p->prev;
+        } // остановились на элементе, который должны удалить*/
+
+        Node* p;
+        stepCount = 0;
+        if (index <= size - 1 - index) // ближе с головы
+        {
+            p = head;
+            for (int i = 0; i < index; i++) {
+                p = p->next;
+                stepCount++;
+            }
+        }
+        else // ближе с хвоста
+        {
+            p = tail;
+            for (int i = size - 1; i > index; i--) {
+                p = p->prev;
+                stepCount++;
+            }
+        }
+        // остановились на элементе, который должны удалить
+
+
+        if (p == head) {
+            head = p->next;
+            p->next->prev = NULL;
+        }
+        else if (p == tail) {
+            tail = p->prev;
+            p->prev->next = NULL;
+        }
+        else {
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+        }
+
+        delete p;
+        size--;
+        return true;
+        
+        /*step = 0;
+        if (!isNumCorrect(n)) return false;
+        Record* nd = enumerate(n);
+        Record* tn = nd->next;
+        Record* tp = nd->prev;
+        if (tp == NULL) first = tn; //óäàëÿåìûé áûë ïåðâûì
+        if (tn == NULL) last = tp; //óäàëÿåìûé áûë ïîñëåäíèì
+        if (tp != NULL) tp->next = tn;//ñóùåñòâóåò ïðåäûäóùèé - óáèðàåì ñâÿçü ñ óäàëÿåìûì
+        if (tn != NULL) tn->prev = tp;//ñóùåñòâóåò ñëåäóþùèé - óáèðàåì ñâÿçü ñ óäàëÿåìûì
+        delete nd; //î÷èñòêà ïàìÿòè
+        size--; //ðàçìåðíîñòü -1
+        return true;*/
+    }
+
+    template <typename T> void List<T>::show() // вывод списка - it should be in main.cpp
+    {
+        if (isEmpty()) return;
+
+        Node* p = head;
+        do {
+            std::cout << p->value << " ::: ";
+            p = p->next;
+        } while (p != NULL);
+        std::cout << std::endl;
+        
+    }
+
+  
+    // обработчики исключений для внешней зоны итератора и проверку для операций при неустановленных итераторах
+    // для * и чтения по номеру обработчики исключений
+
---
+++ main.cpp
@@ -0,0 +1,297 @@
+﻿
+#include <iostream>
+#include "List.cpp"
+using namespace std;
+
+#define Type int
+
+int main() {
+
+	List<Type> list;
+
+	/*list.add(1);
+	list.show();
+
+	list.add(2);
+	list.add(3);
+	list.show();
+
+	list.add(4, 2);
+	list.show();
+	
+	list.add(5, 1);
+	list.show();
+
+	list.add(6, 0);
+	list.show();
+
+	list.add(7, 5);
+	list.show();
+
+	list.add(8, 7);
+	list.show();
+
+	cout << "\n" << list.getSize() << "\n\n";
+
+	list.clear();
+	list.show();
+	if (list.isEmpty()) list.add(9);
+	list.show();
+	if (list.isExisting(9)) list.add(10);
+	list.show();
+
+	List<Type>* list2 = new List<Type>(list);
+	list2->show();
+
+	cout << "\n\n" << list.read(1) << "\n\n";
+
+	list.add(11);
+	list.add(12);
+	list.add(13);
+	list.add(14);
+	list.add(15);
+	list.add(16);
+	list.show();
+
+	cout << "\n\n" << list.read(0);
+	cout << "\n\n" << list.read(7);
+	cout << "\n\n" << list.read(5);
+	cout << "\n\n" << list.read(2);
+	cout << "\n\n" << list.read(6) << "\n\n";
+
+	list.show();
+	list.update(4, 17);
+	list.show();
+	list.update(7, 18);
+	list.show();
+	list.update(0, 19);
+	list.show();
+	list.update(1, 20);
+	list.show();
+
+	cout << "\n" << list.getIndex(20);
+	cout << "\n" << list.getIndex(19);
+	cout << "\n" << list.getIndex(11);
+	cout << "\n" << list.getIndex(18);
+	cout << "\n" << list.getIndex(2) << "\n\n";
+
+	list.show();
+	list.remove(11);
+	list.show();
+	list.remove(12);
+	list.show();
+	list.remove(14);
+	list.show();
+	list.remove(15);
+	list.show();
+
+	list.add(21);
+	list.add(22);
+	list.add(23);
+	list.add(24);
+	cout << "\n\n";
+	list.show();
+
+	list.remove(2, true);
+	list.show();
+	list.remove(0, true);
+	list.show();
+	list.remove(5, true);
+	list.show();
+
+	cout << "\n";
+	for (auto iter = list.begin(); iter != list.end(); ++iter) {
+		std::cout << *iter << std::endl;
+	}
+
+	cout << "\n\n";
+	list.clear();
+	list.show();
+	list.add(25);
+	list.show();
+	list.remove(25);
+	list.show();
+
+	list.add(26);
+	list.add(27);
+	list.add(28);
+	list.add(29);
+	list.show();
+
+	
+	
+	cout << "\n";
+	for (auto iter = list.rbegin(); iter != list.rend(); ++iter) 
+	{
+		std::cout << *iter << std::endl;
+	}*/
+
+	list.add(1);
+	list.add(2);
+	list.add(3);
+
+	
+
+	/*cout << "\n";
+	for (auto iter = list.begin(); iter != list.end(); ++iter) {
+		std::cout << *iter << std::endl;
+	}
+
+	cout << "\n";
+	for (auto iter = list.rbegin(); iter != list.rend(); ++iter) {
+		std::cout << *iter << std::endl;
+	}*/
+
+	try {
+		auto iter = list.begin();
+		cout << *iter;
+		++iter;
+		cout << *iter;
+		++iter;
+		cout << *iter;
+		++iter;
+		cout << *iter;
+	}
+	catch (exception ex) 
+	{
+		cout << ex.what();
+	}
+
+
+}
+/*
+#include <iostream>
+#include "List.cpp"
+using namespace std;
+
+#define Type int
+
+int main() {
+	//system("\chcp 1251");
+	//setlocale(LC_ALL, "rus");
+	//system("cls");
+	List<Type> list;
+	int x, y;
+	auto iter = list.begin();
+	bool isExit = false;
+	cout << "-1. Выход" << endl;
+	cout << "0. Просмотр списка" << endl;
+	cout << "1. Опрос размера списка" << endl;
+	cout << "2. Очистка списка" << endl;
+	cout << "3. Проверка списка на пустоту" << endl;
+	cout << "4. Опрос наличия заданного значения" << endl;
+	cout << "5. Чтение значения с заданным номером в списке" << endl;
+	cout << "6. Изменение значения с заданным номером в списке" << endl;
+	cout << "7. Получение позиции в списке для заданного значения" << endl;
+	cout << "8. Включение нового значения" << endl;
+	cout << "9. Включение нового значения в позицию с заданным номером" << endl;
+	cout << "10. Удаление заданного значения из списка" << endl;
+	cout << "11. Удаление значения из позиции с заданным номером" << endl;
+	cout << "Прямой итератор:" << endl;
+	cout << "\t12. Операция доступа по чтению к текущему значению" << endl;
+	cout << "\t13. Операция доступа по записи к текущему значению" << endl;
+	cout << "\t14. Операция инкремента для перехода к следующему значению в списке" << endl;
+	cout << "\t15. Операция декремента для перехода к предыдущему значению в списке" << endl;
+	cout << "\t16. Проверка равенства однотипных итераторов" << endl;
+	cout << "\t17. Проверка неравенства однотипных итераторов" << endl;
+	cout << "Обратный итератор:" << endl;
+	cout << "\t18. Операция доступа по чтению к текущему значению" << endl;
+	cout << "\t19. Операция доступа по записи к текущему значению" << endl;
+	cout << "\t20. Операция инкремента для перехода к предыдущему значению в списке" << endl;
+	cout << "\t21. Операция декремента для перехода к следующему значению в списке" << endl;
+	cout << "\t22. Проверка равенства однотипных итераторов" << endl;
+	cout << "\t23. Проверка неравенства однотипных итераторов" << endl;
+	cout << "24. Запрос числа элементов списка, просмотренных предыдущей операцией" << endl;
+
+	while (!isExit) {
+		int ans;
+		cin >> ans;
+		switch (ans) {
+		case -1:
+			isExit = true;
+			break;
+		case 0:
+			list.show();
+			break;
+		case 1:
+			cout << list.getSize() << endl;
+			break;
+		case 2:
+			list.clear();
+			cout << "Список очищен" << endl;
+			break;
+		case 3:
+			cout << list.isEmpty() << endl;
+			break;
+		case 4:
+			cout << "Введите значение" << endl;
+			cin >> x;
+			cout << list.isExisting(x) << endl;
+			break;
+		case 5:
+			cout << "Введите номер значения" << endl;
+			cin >> x;
+			y = list.read(x);
+			if ((y > -1) && (y < list.getSize())) cout << y << endl;
+			else cout << "Неверный индекс" << endl;
+			break;
+		case 6:
+			cout << "Введите номер значения" << endl;
+			cin >> x;
+			cout << "Введите новое значение" << endl;
+			cin >> y;
+			cout << list.update(x, y) << endl;
+			break;
+		case 7:
+			cout << "Введите значение" << endl;
+			cin >> y;
+			cout << list.getIndex(y) << endl;
+			break;
+		case 8:
+			cout << "Введите новое значение" << endl;
+			cin >> y;
+			list.add(y);
+			if (list.getSize() == 1) iter = list.begin();
+			break;
+		case 9:
+			cout << "Введите позицию вставки" << endl;
+			cin >> x;
+			cout << "Введите значение" << endl;
+			cin >> y;
+			cout << list.add(x, y) << endl;
+			break;
+		case 10:
+			cout << "Введите значение" << endl;
+			cin >> y;
+			cout << list.remove(y) << endl;
+			break;
+		case 11:
+			cout << "Введите номер значения" << endl;
+			cin >> x;
+			cout << list.remove(x, true) << endl;
+			break;
+		case 12:
+			if (!list.isEmpty()) cout << *iter << endl;
+			else cout << "Список пуст!" << endl;
+			break;
+		case 13:
+			if (!list.isEmpty()) {
+				cout << "Введите значение" << endl;
+				cin >> y;
+				*iter = y;
+			}
+			else cout << "Список пуст!" << endl;
+			break;
+		case 14:
+
+			break;
+
+		case 24:
+			cout << "Число операций = " << list.getStepCount() << endl;
+			break;
+		default:
+			cout << "Введена неверная команда!" << endl;
+		}
+	}
+	return 0;
+}*/
